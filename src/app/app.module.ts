afb89855a8537b133863658bba64e06ddd17cb92
Steven Carroll <scarroll@airpoint.com>
2018-08-03 10:50:45 +0100

    Created app-login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppLoginComponent } from './app-login/app-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent
   ],
   imports: [
     BrowserModule
