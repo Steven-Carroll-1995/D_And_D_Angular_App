b78915a1ac0a950d359dbad897dd02e5080e744a
Steven Carroll <scarroll@airpoint.com>
2018-08-16 16:16:18 +0100

    Redirect to Login Page if user not logged in

--- src/app/_services/login-verification.service.ts
+++ src/app/_services/login-verification.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { UserService } from '../_services/user.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,13 +14,14 @@ export class LoginVerificationService {
   private pwFail: String = 'Wrong Password';
   private tooShort: String = 'Password Too Short';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   verify(username: String, password: String, route: String) {
 
     if (password.length > 3) {
       if (username.toLowerCase() === this.testUsername.toLowerCase()) {
         if (password === this.testPW) {
+            this.userService.logIn();
             this.router.navigate(['/dashboard']);
           } else {
             return this.pwFail;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '../../node_modules/@angular/router';
 import { AppLoginComponent } from './_components/app-login/app-login.component';
 import { DashboardComponent } from './_components/dashboard/dashboard.component';
+import { LoggedInService } from './_services/logged-in.service';
 
 const routes: Routes = [
   { path: 'login', component: AppLoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [LoggedInService] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
